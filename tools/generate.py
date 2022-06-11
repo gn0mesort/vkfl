4218b6fbbc4948251b473a91d8f32e7265e764f6
Alexander Rothman <gnomesort@megate.ch>
2022-06-10 17:19:03 -0700

    Prep for version 2.0.0
    Switch to using Mako templates and defusedxml. Break out some library
    features.

--- tools/generate.py
+++ tools/generate.py
@@ -32,121 +32,13 @@
 # OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-import os
-import sys
-from xml.etree import ElementTree
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from datetime import datetime
 from pathlib import Path
 
-def parse_xml(path: Path):
-    with open(path, 'r') as file:
-        return ElementTree.parse(file)
+from mako.template import Template
 
-def is_descendent(vk_types, name, base):
-    if name == base:
-        return True
-    current = vk_types.get(name)
-    if not current:
-        return False
-    parents = current.get('parent')
-    if not parents:
-        return False
-    return any([ is_descendent(vk_types, parent, base) for parent in parents.split(',') ])
-
-LD_GLOBAL = 0
-LD_INSTANCE = 1
-LD_DEVICE = 2
-
-def identify_group(vk_types, command):
-    vk_global_pfns = set([ 'vkEnumerateInstanceVersion', 'vkEnumerateInstanceExtensionProperties',
-                           'vkEnumerateInstanceLayerProperties', 'vkCreateInstance', 'vkGetInstanceProcAddr' ])
-    name = command.findtext('proto/name')
-    owner = command.findtext('param[1]/type')
-    if name in vk_global_pfns:
-        return LD_GLOBAL
-    if name != 'vkGetDeviceProcAddr' and is_descendent(vk_types, owner, 'VkDevice'):
-        return LD_DEVICE
-    return LD_INSTANCE
-
-def commands_str(commands):
-    result = ''
-    for command in commands:
-        name = command['name']
-        result += f'\t\t{name[2:]},{os.linesep}'
-    return result.strip().expandtabs(2)
-
-def c_commands_str(commands):
-    result = ''
-    for command in commands:
-        name = command['name']
-        result += f'\t\tVKFL_COMMAND_{name[2:]},{os.linesep}'
-    return result.strip().expandtabs(2)
-
-def command_count_str(commands):
-    return str(len(commands))
-
-def defines_str(apis, exts, generate_extra_defines: bool):
-    result = ''
-    for api in apis:
-        symbol = api.replace('VK_VERSION', 'VKFL_API').upper() + '_ENABLED'
-        result += f"#define {symbol} {1 if apis[api]['enabled'] else 0}{os.linesep}"
-    for ext in exts:
-        symbol = ext.replace('VK', 'VKFL', 1).upper()
-        result += f"#define {symbol}_ENABLED {1 if exts[ext]['enabled'] else 0}{os.linesep}"
-        if exts[ext]['enabled'] and generate_extra_defines:
-            sym_name = f'{symbol}_EXTENSION_NAME'
-            result += f'#define {sym_name} \"{ext}\"{os.linesep}'
-            sym_version = f'{symbol}_SPEC_VERSION'
-            result += f"#define {sym_version} {exts[ext]['version']}{os.linesep}"
-    time = datetime.utcnow()
-    result += f'#define VKFL_BUILD_DATE {time.year}{time.month:02}{time.day:02}ULL{os.linesep}'
-    result += f'#define VKFL_BUILD_TIME {time.hour}{time.minute:02}{time.second:02}ULL{os.linesep}'
-    return result.rstrip().expandtabs(2)
-
-def common_private_defines(global_commands, instance_commands, device_commands):
-    result = ''
-    result += f'#define VKFL_GLOBAL_MAX {len(global_commands)}{os.linesep}'
-    result += f'#define VKFL_INSTANCE_MAX (VKFL_GLOBAL_MAX + {len(instance_commands)}){os.linesep}'
-    result += f'#define VKFL_DEVICE_MAX (VKFL_INSTANCE_MAX + {len(device_commands)}){os.linesep}'
-    result += f'#define VKFL_STRING(s) (#s){os.linesep}'
-    return result
-
-def private_defines_str(global_commands, instance_commands, device_commands):
-    result = common_private_defines(global_commands, instance_commands, device_commands)
-    result += f'#define V(cmd) (m_pfns[static_cast<std::size_t>(command::cmd)] = '
-    result += f'context_loader(context, VKFL_STRING(vk##cmd))){os.linesep}'
-    return result.rstrip().expandtabs(2)
-
-def c_private_defines_str(global_commands, instance_commands, device_commands):
-    result = common_private_defines(global_commands, instance_commands, device_commands)
-    result += f'#define V(cmd) (dl->pfns[VKFL_COMMAND_##cmd] = context_loader(context, VKFL_STRING(vk##cmd)))'
-    return result.rstrip().expandtabs(2)
-
-def loader_str(commands, is_macro: bool):
-    result = ''
-    lineend = '\\' if is_macro else ''
-    for command in commands:
-        name = command['name']
-        if name == 'vkGetInstanceProcAddr':
-            continue
-        result += f'\t\tV({name[2:]});{lineend}{os.linesep}'
-    return result.rstrip().expandtabs(2)
-
-def header_version_str(tree):
-    return tree.find('types/type[name="VK_HEADER_VERSION"]').find('name').tail.strip()
-
-def enabled_by_any(check_features: set[str], features: dict):
-    res = False
-    for key in check_features:
-        res = res or features[key]['enabled']
-    return res
-
-def version_cmp(ver1: list[str], ver2: list[str]):
-    major = int(ver1[0]) - int(ver2[0])
-    if major == 0:
-        return int(ver1[1]) - int(ver2[1])
-    return major
+from generatorlib.vulkan import parse_vulkan_spec, VulkanVersion, VulkanCommandType
 
 def comma_separated(s: str):
     res = set()
@@ -155,24 +47,6 @@ def comma_separated(s: str):
             res.add(value.replace('\"', '').replace('\'', ''))
     return res
 
-def spec_path(search_paths: list[Path]):
-    for search_path in search_paths:
-        if not search_path:
-            continue
-        registry = search_path.absolute().joinpath('share/vulkan/registry/vk.xml')
-        if registry.exists() and registry.is_file():
-            return registry
-    return Path.cwd().joinpath('vk.xml').absolute()
-
-default_spec = ''
-if sys.platform == 'win32' or sys.platform == 'cygwin':
-    default_spec = spec_path([ os.environ['VULKAN_SDK'], os.environ['VULKAN_SDK_PATH'] ])
-# It's not clear to me that this is adequate with regard to Apple environments. Documentation for the LunarG
-# Vulkan SDK indicates that the registry should be located at $VULKAN_SDK/share/registry/vk.xml as it is on Linux.
-# Hopefully, using the same search as a Linux system is adequate.
-else:
-    default_spec = spec_path([ Path(os.environ['VULKAN_SDK']), Path.home().joinpath('.local'), Path('/usr/local'),
-                               Path('/usr') ])
 epilog = """
 If a specification path is not explicitly provided, the following locations are searched.
 Unix-like systems:
@@ -180,116 +54,78 @@ Unix-like systems:
 \t$HOME/.local/share/vulkan/registry/vk.xml
 \t/usr/local/share/vulkan/registry/vk.xml
 \t/usr/share/vulkan/registry/vk.xml
-\t$PWD/vk.xml
 Windows systems:
 \t%VULKAN_SDK%/share/vulkan/registry/vk.xml
 \t%VULKAN_SDK_PATH%/share/vulkan/registry/vk.xml
-\t%CD%/vk.xml
 """
 parser = ArgumentParser(epilog=epilog, formatter_class=RawDescriptionHelpFormatter)
-parser.add_argument('--spec', default=default_spec, type=Path, help='A file path to an XML specification of the ' +
-                    'Vulkan API. If this isn\'t provided, the script will default to searching a set of standard ' +
-                    'paths for vk.xml.')
-parser.add_argument('--extensions', type=comma_separated, default='all',
-                    help='A comma separated list of Vulkan extensions to include in the loader. ' +
-                         'This may also be the special value \"all\". Defaults to \"all\".')
-parser.add_argument('--api', default='latest',
-                    help='The latest Vulkan API version to include in the loader (i.e. 1.0, 1.1, 1.2, etc.). ' +
-                         'This may also be the special value \"latest\". Defaults to \"latest\".')
-parser.add_argument('--generate-extra-defines', const=True, action='store_const', default=False,
-                    help='Enable the generation of \"VKFL_X_EXTENSION_NAME\" and \"VKFL_X_SPEC_VERSION\" symbols. ' +
-                         'This is disabled by default.')
-parser.add_argument('INPUT', type=Path, help='A path to an input template file.')
-parser.add_argument('OUTPUT', type=Path, help='A path to an output file.')
+parser.add_argument("--spec", type=Path, help="A file path to an XML specification of the " +
+                    "Vulkan API. If this isn't provided, the script will default to searching a set of standard " +
+                    "paths for vk.xml.", default=None)
+parser.add_argument("--extensions", type=comma_separated, default=["all"],
+                    help="A comma separated list of Vulkan extensions to include in the loader. " +
+                         "This may also be the special value \"all\". Defaults to \"all\".")
+parser.add_argument("--api", default="latest",
+                    help="The latest Vulkan API version to include in the loader (i.e. 1.0, 1.1, 1.2, etc.). " +
+                         "This may also be the special value \"latest\". Defaults to \"latest\".")
+parser.add_argument("--no-generate-disabled-defines", const=False, action="store_const", default=True,
+                    help="Disable the generation of symbols indicating that an extension or API version is disabled" +
+                         " (i.e., don't generate VKFL_X_ENABLED if it would be defined as 0).")
+parser.add_argument("INPUT", type=Path, help="A path to an input template file.")
+parser.add_argument("OUTPUT", type=Path, help="A path to an output file.")
 args = parser.parse_args()
-tree = parse_xml(args.spec)
-input_exts = args.extensions
-use_all_exts = False
-if 'all' in input_exts:
-    use_all_exts = True
-exts = { }
-api_version = None
-use_latest_api = False
-if args.api == 'latest':
-    use_latest_api = True
+(apis, extensions, commands, spec_version) = parse_vulkan_spec(args.spec)
+enabled_extensions = None
+disabled_extensions = { }
+if "all" in args.extensions:
+    enabled_extensions = extensions
 else:
-    api_version = args.api.split('.')
-generate_extra_defines = args.generate_extra_defines
-apis = { }
-vk_types = { }
-for vk_type in tree.findall('types/type'):
-    name = vk_type.findtext('name')
-    vk_types[name] = vk_type
-vk_commands = { }
-for vk_command in tree.findall('commands/command'):
-    alias = vk_command.get('alias')
-    name = vk_command.get('name') if alias else vk_command.findtext('proto/name')
-    vk_commands[name] = { }
-    vk_commands[name]['name'] = name
-    vk_commands[name]['element'] = vk_command
-    vk_commands[name]['used_by_apis'] = set()
-    vk_commands[name]['used_by_exts'] = set()
-    ident_node = None
-    if alias:
-        vk_commands[name]['is_alias'] = True
-        ident_node = tree.find(f'commands/command/proto/name[.="{alias}"]/../..')
-    else:
-        vk_commands[name]['is_alias'] = False
-        ident_node = vk_command
-    vk_commands[name]['loaded_from'] = identify_group(vk_types, ident_node)
-for vk_feature in tree.findall('feature'):
-    name = vk_feature.get('name')
-    version = vk_feature.get('number').split('.')
-    apis[name] = { 'enabled': False }
-    if use_latest_api or version_cmp(version, api_version) <= 0:
-        apis[name]['enabled'] = True
-    for command in vk_feature.findall('require/command'):
-        command_name = command.get('name')
-        vk_commands[command_name]['used_by_apis'].add(name)
-for vk_extension in tree.findall('extensions/extension'):
-    name = vk_extension.get('name')
-    exts[name] = { 'enabled': False, 'version': 0 }
-    if use_all_exts or name in input_exts:
-        exts[name]['enabled'] = True
-    for requirement in vk_extension.findall('require'):
-        for enum in requirement.findall('enum'):
-            if enum.get('name') == f'{name.upper()}_SPEC_VERSION':
-                value = enum.get('value')
-                exts[name]['version'] = int(value) if value else 0
-        for command in requirement.findall('command'):
-            command_name = command.get('name')
-            vk_commands[command_name]['used_by_exts'].add(name)
-vk_global_commands = [ ]
-vk_instance_commands = [ ]
-vk_device_commands = [ ]
-for vk_command in vk_commands:
-    command = vk_commands[vk_command]
-    if enabled_by_any(command['used_by_apis'], apis) or enabled_by_any(command['used_by_exts'], exts):
-        if command['loaded_from'] == LD_GLOBAL:
-            vk_global_commands.append(command)
-        elif command['loaded_from'] == LD_INSTANCE:
-            vk_instance_commands.append(command)
-        elif command['loaded_from'] == LD_DEVICE:
-            vk_device_commands.append(command)
-key_fn = lambda command: command['name']
-vk_all_commands = sorted(vk_global_commands, key=key_fn) + sorted(vk_instance_commands, key=key_fn)
-vk_all_commands += sorted(vk_device_commands, key=key_fn)
-text = ''
-with open(args.INPUT, 'rb') as file:
-    text = str(file.read(), 'utf-8')
-text = text.replace('@commands@', commands_str(vk_all_commands))
-text = text.replace('@commands_count@', command_count_str(vk_all_commands))
-text = text.replace('@header_version@', header_version_str(tree))
-text = text.replace('@load_global@', loader_str(vk_global_commands, False))
-text = text.replace('@load_instance@', loader_str(vk_instance_commands, False))
-text = text.replace('@load_device@', loader_str(vk_device_commands, False))
-text = text.replace('@load_device_macro@', loader_str(vk_device_commands, True))
-text = text.replace('@defines@', defines_str(apis, exts, generate_extra_defines))
-text = text.replace('@private_defines@',
-                    private_defines_str(vk_global_commands, vk_instance_commands, vk_device_commands))
-text = text.replace('@c_commands@', c_commands_str(vk_all_commands))
-text = text.replace('@c_private_defines@', c_private_defines_str(vk_global_commands, vk_instance_commands,
-                                                                 vk_device_commands))
-with open(args.OUTPUT, 'wb') as file:
-    file.truncate()
-    file.write(bytes(text, 'utf-8'))
+    enabled_extensions = { }
+    for extension in extensions:
+        if extension in args.extensions:
+            enabled_extensions[extension] = extensions[extension]
+        else:
+            disabled_extensions[extension] = extensions[extension]
+enabled_apis = None
+disabled_apis = { }
+if args.api == "latest":
+    enabled_apis = apis
+else:
+    enabled_apis = { }
+    desired = VulkanVersion(args.api)
+    for api in apis:
+        if apis[api].version() <= desired:
+            enabled_apis[api] = apis[api]
+        else:
+            disabled_apis[api] = apis[api]
+enabled_global_commands = [ ]
+enabled_instance_commands = [ ]
+enabled_device_commands = [ ]
+for api in enabled_apis:
+    for command in enabled_apis[api].commands():
+        command_type = commands[command].type()
+        if command_type == VulkanCommandType.GLOBAL:
+            enabled_global_commands.append(command)
+        elif command_type == VulkanCommandType.INSTANCE:
+            enabled_instance_commands.append(command)
+        elif command_type == VulkanCommandType.DEVICE:
+            enabled_device_commands.append(command)
+for extension in enabled_extensions:
+    for command in enabled_extensions[extension].commands():
+        command_type = commands[command].type()
+        if command_type == VulkanCommandType.GLOBAL:
+            enabled_global_commands.append(command)
+        elif command_type == VulkanCommandType.INSTANCE:
+            enabled_instance_commands.append(command)
+        elif command_type == VulkanCommandType.DEVICE:
+            enabled_device_commands.append(command)
+template = Template(filename=args.INPUT.as_posix(), output_encoding="utf-8")
+source = template.render(enabled_apis=enabled_apis, disabled_apis=disabled_apis,
+                         enabled_extensions=enabled_extensions, disabled_extensions=disabled_extensions,
+                         enabled_global_commands=enabled_global_commands,
+                         enabled_instance_commands=enabled_instance_commands,
+                         enabled_device_commands=enabled_device_commands, spec_version=spec_version,
+                         generate_disabled_defines=args.no_generate_disabled_defines,
+                         buildtime=datetime.utcnow())
+with open(args.OUTPUT, "wb") as file:
+    file.write(source)
---
+++ tools/generatorlib/vulkan.py
@@ -0,0 +1,169 @@
+import os
+import sys
+from enum import Enum
+from pathlib import Path
+
+from defusedxml import ElementTree
+
+class VulkanVersion:
+    def __init__(self, version: str):
+        parts = version.strip().split('.')
+        assert len(parts) >= 1
+        self._major = int(parts[0])
+        if len(parts) == 1:
+            self._minor = 0
+        else:
+            self._minor = int(parts[1])
+
+    def major(self) -> int:
+        return self._major
+
+    def minor(self) -> int:
+        return self._minor
+
+    def compare(self, other) -> int:
+        res = self._major - other._major
+        if res == 0:
+            res = self._minor - other._minor
+        return res
+
+    def __eq__(self, other) -> bool:
+        return self.compare(other) == 0
+
+    def __lt__(self, other) -> bool:
+        return self.compare(other) < 0
+
+    def __le__(self, other) -> bool:
+        return self.compare(other) <= 0
+
+    def __str__(self) -> str:
+        return f"{self._major}.{self._minor}"
+
+class VulkanCommandType(Enum):
+    GLOBAL = 0,
+    INSTANCE = 1,
+    DEVICE = 2
+
+class VulkanCommand:
+    def __init__(self, tree, node):
+        self._original_node = node
+        aliased_command = node.get("alias")
+        if aliased_command:
+            self._name = node.get("name")
+            node = tree.find(f"commands/command/proto/name[.='{aliased_command}']/../..")
+        else:
+            self._name = node.findtext("proto/name")
+        assert self._name.startswith("vk")
+        global_fns = ("vkEnumerateInstanceVersion", "vkEnumerateInstanceExtensionProperties",
+                      "vkEnumerateInstanceLayerProperties", "vkCreateInstance", "vkGetInstanceProcAddr")
+        if self._name in global_fns:
+            self._type = VulkanCommandType.GLOBAL
+        else:
+            owner = node.findtext("param[1]/type")
+            if owner in ("VkInstance", "VkPhysicalDevice"):
+                self._type = VulkanCommandType.INSTANCE
+            elif owner in ("VkDevice", "VkCommandBuffer", "VkQueue"):
+                self._type = VulkanCommandType.DEVICE
+            else:
+                assert False
+        self._node = node
+
+    def name(self) -> str:
+        return self._name
+
+    def type(self) -> VulkanCommandType:
+        return self._type
+
+    def node(self):
+        return self._node
+
+class VulkanFeature:
+    def __init__(self, node):
+        self._node = node
+        if node.tag == "feature":
+            self._version = VulkanVersion(node.get("number"))
+            self._name = node.get("number")
+        else:
+            self._name = node.get("name")
+            version_node = node.find(f"require/enum[@name='{self._name.upper()}_SPEC_VERSION']")
+            # This is inexplicable to me. Originally, I tried if version_node but that doesn't seem to work.
+            if version_node is not None and version_node.get("value") is not None:
+                self._version = VulkanVersion(version_node.get("value"))
+            else:
+                # print(f"Bad extension: {self._name}")
+                self._version = VulkanVersion("0.0")
+        self._enabled = False
+        self._commands = set()
+        for command in node.findall("require/command"):
+            self._commands.add(command.get("name"))
+
+    def node(self):
+        return self._node
+
+    def name(self) -> str:
+        return self._name
+
+    def version(self) -> VulkanVersion:
+        return self._version
+
+    def enabled(self) -> bool:
+        return self._enabled
+
+    def enable(self):
+        self._enabled = True
+
+    def disable(self):
+        self._enabled = False
+
+    def commands(self) -> set[str]:
+        return self._commands
+
+def _find_spec(search_paths: list[Path]) -> Path:
+    for search_path in search_paths:
+        complete_path = search_path.joinpath("share/vulkan/registry/vk.xml").absolute()
+        if complete_path.exists() and complete_path.is_file():
+            return complete_path
+    return None
+
+def _parse_commands(spec_tree):
+    res = { }
+    for command in spec_tree.findall("commands/command"):
+        parsed = VulkanCommand(spec_tree, command)
+        res[parsed.name()] = parsed
+    return res
+
+def _parse_apis(spec_tree):
+    res = { }
+    for feature in spec_tree.findall("feature"):
+        parsed = VulkanFeature(feature)
+        res[parsed.name()] = parsed
+    return res
+
+def _parse_extensions(spec_tree):
+    res = { }
+    for feature in spec_tree.findall("extensions/extension"):
+        parsed = VulkanFeature(feature)
+        res[parsed.name()] = parsed
+    return res
+
+
+def parse_vulkan_spec(spec_path: Path):
+    if spec_path is None:
+        if ((sys.platform == "win32" or sys.platform == "cygwin") and
+            ("VULKAN_SDK" in os.environ or "VULKAN_SDK_PATH" in os.environ)):
+            spec_path = _find_spec([ Path(os.environ["VULKAN_SDK"]), Path(os.environ["VULKAN_SDK_PATH"]) ])
+        else:
+            search_paths = [ ]
+            if "VULKAN_SDK" in os.environ:
+                search_paths.append(Path(os.environ["VULKAN_SDK"]))
+            search_paths.extend([ Path.home().joinpath(".local"), Path("/usr/local"), Path("/usr") ])
+            spec_path = _find_spec(search_paths)
+    spec_tree = None
+    with open(spec_path, "r") as spec_file:
+        spec_tree = ElementTree.parse(spec_file)
+    commands = _parse_commands(spec_tree)
+    apis = _parse_apis(spec_tree)
+    extensions = _parse_extensions(spec_tree)
+    spec_version = spec_tree.find("types/type[name='VK_HEADER_VERSION']").find("name").tail.strip()
+    return (apis, extensions, commands, spec_version)
+
